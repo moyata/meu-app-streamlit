75168c39496d9470804b75dee57fb49278b104a4
Marcelo Oyata <moyata@hotmail.com>
2025-03-05 16:17:12 -0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 
-API_KEY = "oflivros@oficinadelivros.iam.gserviceaccount.com"  # Chave do Gepeto 🔑
+API_KEY = "AIzaSyANfUXUjmQv-Pem9AZyGpziAB2ZJ0XrScM"  # Chave do Gepeto 🔑
 
 # Função para buscar dados pela API do Google Books
 def buscar_google_books(isbn):
